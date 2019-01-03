004bbf137846701e4eccb95530158250cf8d0573
Freddy Castillo <freddy.geovanni@gmail.com>
2019-01-03 00:27:03 -0500

    Estroctura del Proyecto, LOgo, Header

---
+++ platziVideoApp/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+import Home from "./src/screens/containers/home";
+import Header from "./src/sections/components/header";
+
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Home>
+        <Header/>
+        <Text>Buscador</Text>
+        <Text>Categorias</Text>
+        <Text>Sugerencias</Text>
+        
+      </Home>
+    );
+  }
+}
