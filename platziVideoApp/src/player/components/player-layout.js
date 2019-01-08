f5d760edbadd72a071f7c7e9294d0687738d2472
Freddy Castillo <freddy.geovanni@gmail.com>
2019-01-08 00:16:54 -0500

    LOader

--- platziVideoApp/src/player/components/player-layout.js
+++ platziVideoApp/src/player/components/player-layout.js
@@ -1,24 +1,38 @@
 import React from "react";
-import {View,Text,StyleSheet} from "react-native";
-function VideoLayout(props){
+import { View, Text, StyleSheet } from "react-native";
+function VideoLayout(props) {
     return (
-        <View style={style.container}> 
+        <View style={style.container}>
             <View style={style.video}>
-               {props.video} 
+                {props.video}
             </View>
-            {props.loader}
+            <View style={style.loader}>
+                {props.loading && props.loader}
+            </View>
         </View>
     )
 }
-const style= StyleSheet.create({
-    container:{paddingTop:"56.25%"},
-    video:{
-        position:"absolute",
-        left:0,
-        top:0,
-        bottom:0,
-        right:0,
-        backgroundColor:"black"
+const style = StyleSheet.create({
+    container: {
+        paddingTop: "56.25%"
+    },
+    video: {
+
+        position: "absolute",
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        backgroundColor: "black"
+    },
+    loader: {
+        position: "absolute",
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        justifyContent: "center",
+        alignItems: "center"
     },
 })
 export default VideoLayout;
--- platziVideoApp/src/player/containers/player.js
+++ platziVideoApp/src/player/containers/player.js
@@ -3,11 +3,24 @@ import Video from "react-native-video";
 import { StyleSheet, View ,ActivityIndicator} from "react-native";
 import PLayerLayout from "../components/player-layout"
 class PLayer extends Component {
+    state={
+        loading:true,
+    }
+    onBuffer=({isBuffering})=>{
+        this.setState({
+            loading:isBuffering
+        })
+    }
+    onLoad=()=>{
+        this.setState({
+            loading:false
+        })
+    }
     render() {
         return (
-            <PLayerLayout video={
+            <PLayerLayout loading={this.state.loading} video={
                 <Video playWhenInactive={true} source={{ uri: "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4" }}
-                    style={style.video} resizeMode="contain" />
+                    style={style.video} resizeMode="contain"  onBuffer={this.onBuffer}  onLoad={this.onLoad}/>
             }
             loader={
                 <ActivityIndicator color="white"/>
